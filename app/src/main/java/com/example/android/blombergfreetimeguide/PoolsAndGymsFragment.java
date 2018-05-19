30495e8d9563ffd2780c412feefcebcd11a15993
northyde <northyde@yahoo.de>
2018-05-20 00:17:50 +0200

    feat: photos

--- app/src/main/java/com/example/android/blombergfreetimeguide/PoolsAndGymsFragment.java
+++ app/src/main/java/com/example/android/blombergfreetimeguide/PoolsAndGymsFragment.java
@@ -28,7 +28,18 @@ public class PoolsAndGymsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.places_list, container, false);
 
         final ArrayList<Place> places = new ArrayList<Place>();
-        places.add(new Place("Test Schwimmhalle", "Alte Chaussee 8", "Donop"));
+        places.add(new Place("Outdoor Pool", "Wesselweg 28", "Blomberg"));
+places.add(new Place("Indoor Pool", "Ulmenallee", "Blomberg"));
+places.add(new Place("Gymnasium Grundschule am Paradies", "Holstenhöfener Straße 4", "Blomberg"));
+places.add(new Place("Gymnasium Grundschule am Weinberg", "Friedrichstraße ", "Blomberg"));
+places.add(new Place("Gymnasium Grundschule Reelkirchen", "Fasanenweg 3 ", "Reelkirchen"));
+places.add(new Place("Gymnastics gymnasium", "Ostring 14", "Blomberg"));
+places.add(new Place("Multi-purpose hall Istrup", "Detmolder Straße", "Istrup"));
+places.add(new Place("Sportshall I im Schulzentrum", "Ulmenallee 11", "Blomberg"))
+;places.add(new Place("Sportshall II im Schulzentrum", "Ulmenallee 11", "Blomberg"));
+places.add(new Place("Gymnasium ehemalige Grundschule am Paradies", "Paradies 5", "Blomberg"));
+places.add(new Place("Gymnasium Grundschule am Weinberg", "Friedrichstraße", "Blomberg"));
+places.add(new Place("Gymnasium Grundschule Großenmarpe", "Turnhallenstraße", "Großenmarpe"));
 
         PlacesAdapter adapter = new PlacesAdapter(getActivity(), places, R.color.category_PoolsAndGyms);
 
--- app/src/main/java/com/example/android/blombergfreetimeguide/SportsFieldsFragment.java
+++ app/src/main/java/com/example/android/blombergfreetimeguide/SportsFieldsFragment.java
@@ -28,7 +28,24 @@ public class SportsFieldsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.places_list, container, false);
 
         final ArrayList<Place> places = new ArrayList<Place>();
-        places.add(new Place("Test Bolzplatz", "Alte Chaussee 8", "Donop"));
+        places.add(new Place("Playingfield Oberes Bruch", "Paradies", "Blomberg"));
+places.add(new Place("Playingfield Brüntrup", "Wehrener Straße", "Brüntrup"));
+places.add(new Place("Playingfield Cappel", "Am Friedhof", "Cappel"));
+places.add(new Place("Playingfield Donop", "Im Ort", "Donop"));
+places.add(new Place("Playingfield Großenmarpe", "Am Sportplatz", "Großenmarpe"));
+places.add(new Place("Playingfield Istrup", "Detmolder Straße", "Istrup"));
+places.add(new Place("Stadium Am Rammbocke", "Am Rammbocke 20", "Blomberg"));
+places.add(new Place("Track and Field Schulzentrum", "Ulmenallee", "Blomberg"));
+places.add(new Place("Football ground Bexten", "Bextenstraße", "Blomberg"));
+places.add(new Place("Football ground Eschenbruch", "Winterbergstraße", "Eschenbruch"));
+places.add(new Place("Football ground Flachsmarkt", "Flachsmarktstraße", "Blomberg"));
+places.add(new Place("Football ground Gergeloh", "Gergeloh", "Blomberg"));
+places.add(new Place("Football ground Grundschule Reelkirchen", "Fasanenweg", "Reelkirchen"));
+places.add(new Place("Football ground Grunewalder Straße", "Grunewalder Straße", "Blomberg"));
+places.add(new Place("Football ground Jugendzentrum", "Am Paradies", "Blomberg"));
+places.add(new Place("Football ground Tintrup", "Maper Straße", "Tintrup"));
+places.add(new Place("Football ground Wellentrup", "Am Mühlenfeld", "Wellentrup"));
+places.add(new Place("Football ground Cappel", "Lilienweg", "Cappel"));
 
         PlacesAdapter adapter = new PlacesAdapter(getActivity(), places, R.color.category_SportsFields);
 
